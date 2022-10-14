ae810b75c222e20d4d5a1a15dd01831573e59e31
vishnu prakash <vishnuprakash71298@gmail.com>
2022-10-14 21:17:09 +0530

    Add webpage files

---
+++ script.js
@@ -0,0 +1,98 @@
+
+var Lists = JSON.parse(document.getElementById('Lists').value);
+var content = document.getElementById('list_body').innerHTML;
+var delete_id = '';
+
+Lists.forEach(list => {
+  let value = `<tr> 
+  <th scope="row">${list.id}</th><td>${list.name}</td><td>${list.price}</td><td>${list.qty}</td><td>${list.status}</td>
+  <td>
+  <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editProduct"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+  </svg></button></td>
+
+  <td>
+  <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#deleteProduct" id="deleteButton" data-id = ${list.id}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+  </svg> </button>
+  </td>
+  
+  </tr>`;
+  content = content + value;
+})
+document.getElementById('list_body').innerHTML = content;
+
+document.querySelector('#create_save').addEventListener('click', () => {
+  let id = Lists.length + 1
+  let name = document.getElementById('create_name').value;
+  let amount = document.getElementById('create_amount').value;
+  let qty = document.getElementById('create_qty').value;
+  let status = document.getElementById('create_status').value;
+
+  if(name !== '' && amount !== '' && qty !== '' && status !== '') {
+    Lists.push({
+      id: id,
+      name: name,
+      price: amount,
+      qty: qty,
+      status: status
+    });
+    document.getElementById('Lists').value = JSON.stringify(Lists);
+    document.getElementById('create_success').style.display = 'block';
+    let value = `<tr> 
+    <th scope="row">${id}</th><td>${name}</td><td>${amount}</td><td>${qty}</td><td>${status}</td>
+    <td>
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editProduct"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+    </svg></button></td>
+
+    <td>
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#deleteProduct" id="deleteButton" data-id = ${id}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+    </svg> </button>
+  </td></tr>`;
+    content = content + value;
+    document.getElementById('list_body').innerHTML = content;
+    document.getElementById('create_buttonClose').click();
+  }
+})
+
+if(document.querySelectorAll('#deleteButton')) {
+  var deleteContent = document.querySelectorAll('#deleteButton');
+  deleteContent.forEach(selectedProduct => selectedProduct.addEventListener('click', () => {
+   delete_id = selectedProduct.dataset.id;
+  }));
+}
+
+if(document.querySelectorAll('#delete_confirm')) {
+  document.querySelector('#delete_confirm').addEventListener('click', () => {
+    var index  = 0;
+    Lists.forEach(function(list, i)  {
+      if((list.id).toString() === delete_id) {
+        index = i;
+        return;
+      }
+    })
+    delete Lists[index];
+    content = '';
+    Lists.forEach(list => {
+      let value = `<tr> 
+      <th scope="row">${list.id}</th><td>${list.name}</td><td>${list.price}</td><td>${list.qty}</td><td>${list.status}</td>
+      <td>
+      <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editProduct"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+      <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+      </svg></button></td>
+    
+      <td>
+      <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#deleteProduct" id="deleteButton" data-id = ${list.id}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+      </svg> </button>
+      </td>
+      
+      </tr>`;
+      content = content + value;
+    })
+    document.getElementById('list_body').innerHTML = content;
+    document.getElementById('delete_clodeButton').click();
+  })
+}
